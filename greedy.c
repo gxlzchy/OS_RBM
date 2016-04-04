7ce0cd93ba812ef8452d53b89411e1da72d1cd45
Andy Chen <gxlzchy@gmail.com>
2016-04-04 16:50:44 +0800

    Change the N into NumOfReq in line 9 of FCFS. Add Greedy Function with relation to FCFS function. Add a stronger test case.

--- greedy.c
+++ greedy.c
@@ -4,35 +4,14 @@
 
 #define N 20  //The maximun acceptable number of request.
 #define M 100 //The maximun duration of all requests is two weeks time: 2*5(per week)*9(9am-6pm)=90.
-
-
-/**
-	@cmp: compare two variable to prepare for qsort in greedy()
-*/
-int cmp ( const void *pa, const void *pb ) {
-    const int (*a)[3] = pa;
-    const int (*b)[3] = pb;
-
-    if ( (*a)[2] > (*b)[2] ) return -1;
-    if ( (*a)[2] < (*b)[2] ) return +1;
-    if ( (*a)[2] == (*b)[2] ){
-    	if ( (*a)[1] < (*b)[1] ) return -1;
-    	if ( (*a)[1] > (*b)[1] ) return +1;
-    	return 0;
-    }
-}
-
-
-
-
-
 /**@param 
 		reqNum: int[] reqNum[index]=the request number. 
+!!!!!!!!!!!!Warning: The reqNum array should be in order.
 		numOfReq: int, the number of request needed to be proceeded by FCFS. 
         st: int[]  st[reqNum]: the start time of the requests. 
         ed: int[]. ed[reqNum]: the end time of the requests. 
-		End time is inclusive.
-        fNum: int[][]:
+!!!!!!!!!!!!Warning: End time is included.
+        fNum: int[][] 
          	fNum[reqNum][0]=The total number facilities in this request
          	fNum[reqNum][i]=facility Number.(i=1..3)
    @the function takes in array of requests and schedule it with First Come First Serve algorithm
@@ -40,6 +19,7 @@ int cmp ( const void *pa, const void *pb ) {
    second one 1:accepted, 0:rejected.
    The first dimension is just for indexing different requests
 */
+
 int** FCFS(int reqNum[], int numOfReq, int st[], int ed[],int fNum[][5]){
 	int i,j,k;
 
@@ -86,20 +66,20 @@ int** FCFS(int reqNum[], int numOfReq, int st[], int ed[],int fNum[][5]){
     return reqStatus;
 }
 
-/**@param 
-		reqNum: int[] reqNum[index]=the request number. 
-		numOfReq: int, the number of request needed to be proceeded by FCFS. 
-        st: int[]  st[reqNum]: the start time of the requests. 
-        ed: int[]. ed[reqNum]: the end time of the requests. 
-		End time is inclusive.
-        fNum: int[][]:
-         	fNum[reqNum][0]=The total number facilities in this request
-         	fNum[reqNum][i]=facility Number.(i=1..3)
-   @the function takes in array of requests and schedule it with greedy algorithm
-   @return: returns a 2D array. the second dimension has 2 variables, first one is the request number
-   second one 1:accepted, 0:rejected.
-   The first dimension is just for indexing different requests
-*/
+int cmp ( const void *pa, const void *pb ) {
+    const int (*a)[3] = pa;
+    const int (*b)[3] = pb;
+
+    if ( (*a)[2] > (*b)[2] ) return -1;
+    if ( (*a)[2] < (*b)[2] ) return +1;
+    if ( (*a)[2] == (*b)[2] ){
+    	if ( (*a)[1] < (*b)[1] ) return -1;
+    	if ( (*a)[1] > (*b)[1] ) return +1;
+    	return 0;
+    }
+}
+
+
 int** greedy(int reqNum[], int numOfReq, int st[], int ed[],int fNum[][5]){
 	int i,j,k;
 
@@ -133,119 +113,26 @@ int** greedy(int reqNum[], int numOfReq, int st[], int ed[],int fNum[][5]){
 	
 }
 
-
-/** function input
-@param: int reqno:to let the function know how many request is recieved now
-variables: char input for storing the line user entered
-           int firstword to save the place of first word
-		   char *fac for saving the facility user entered
-		   char *dat for the date 
-		   char* time for the time
-		   char *dur for the duration
-		   char * caller for the caller
-	@the function reads a line of input and detect which request has been entered
-	@This function return an array for saving input from user 
-	place of array:0:requestNumber,1: starttime,2:endtime,3:
-	
-		   
-*/
-int* input(int reqno)
-{
-char input[30];/* for storing everyting user input*/
-int i,n;/* to store the length of the first word(request)*/
-const char space[2]=" ";
-const char *a="a";
-const char *p="p";
-const char *e="e";
-const char *b="b";
-char *firstword;
-char first[2], third[2];
-int *result;
-printf("please enter a booking:\n");
-scanf("%s",&input);
-firstword = strtok(input,space);
-sprintf(first, "%c",firstword[0]);
-sprintf(third,"%c",firstword[3]);
-if (strcmp(first,a)==0){/* for the case add booking command is entered*/
-	if (strcmp(third,b)!=0){
-		char *fac,*dat,*time,*dur,*caller,*dev,*dev1;
-		fac = strtok(NULL,space);
-		dat = strtok(NULL,space);
-		time = strtok(NULL,space);
-		dur = strtok(NULL,space);
-		caller = strtok(NULL,space);
-		n=5;
-		result = malloc(5*sizeof(int));
-	}
-	request[0]=reqno;
-	
-	printf("a request of booking is recieved\n");
-	switch (firstword[3]){
-		case 'M':
-		case 'm':
-			result[3]=2;
-			switch (fac[5]){
-				case 'A':
-					result[4]=1;
-				case 'B':
-					result[4]=2;
-			}
-			
-			printf("the request is addMeeting\n");
-			break;
-		case 'p':
-		case 'P':
-			n=7;
-			printf("the request is addPresentation\n");
-			break;
-		case 'C':
-		case 'c':
-			n=7;
-			printf("the request is addConference\n");
-			break;
-		case 'D':
-		case 'd':
-			printf("the request is addDevice\n");
-			break;
-		case 'B':
-		case 'b':
-			printf("the request is addBatch\n");
-			break;
-		}
-	
-	}
-	else if (strcmp(first,p)==0){
-		/*call output and calaulate*/
-		printf("print out booking\n");
-	}	
-	else {
-		/*user input error*/
-		printf("unknown request");
-		}
-	
-	return result;
-}
-
-
-
 int main(){
 	// 1. input module - input lines in cmd and lines in .dat files
 	int reqNum[N],st[N],ed[N],fNum[N][5];
 	int i,reqno;
 	
 	// 2. scheduling module - comment your updated interface if there is any modification
-
 	///*for testing
+	//#1
 	reqNum[0]=1;
 	st[1]=1;
 	ed[1]=5;
 	fNum[1][0]=2;fNum[1][1]=1;fNum[1][2]=2;
 
+	//#2
 	reqNum[1]=2;
 	st[2]=2;
 	ed[2]=4;
 	fNum[2][0]=1;fNum[2][1]=2;
 
+	//#3
 	reqNum[2]=3;
 	st[3]=3;
 	ed[3]=3;
@@ -291,12 +178,12 @@ int main(){
 	ed[11]=1;
 	fNum[11][0]=1;fNum[11][1]=2;
 
+
 	//*/
-	
-	// 3. output module - print out the latest schedule and write into .dat files
-	// parent and child processes
 
 	int **reqStatus=greedy(reqNum,11,st,ed,fNum);
+
+
 	for (i=0;i<11;i++)
 		printf("request #%d %s\n", reqStatus[i][0],reqStatus[i][1]==1?"accept":"reject");
 	
--- testing.c
+++ testing.c
@@ -1,20 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-int** function()
-{
-    int ** a = (int **)malloc(50*sizeof(int*));
-    int i;
-    for (i=0;i<40;i++)
-    	a[i]=(int*)malloc(3*sizeof(int));
-    
-    a[1][1]=9;
-    return a;
+#define MAX 10
+void print ( int a[][4] ) {
+    int r,c;
+    for ( r = 0 ; r < MAX ; r++ ) {
+        for ( c = 0 ; c < 4 ; c++ ) {
+            printf( "%d ", a[r][c] );
+        }
+        printf( "\n" );
+    }
 }
-
-int main()
-{
-    int** a = function();
-    printf("%d\n", a[1][1]);
+ 
+int cmp ( const void *pa, const void *pb ) {
+    const int (*a)[4] = pa;
+    const int (*b)[4] = pb;
+    if ( (*a)[3] < (*b)[3] ) return -1;
+    if ( (*a)[3] > (*b)[3] ) return +1;
+    return 0;
+}
+ 
+int main ( ) {
+    int a[MAX][4];
+    int r,c;
+    for ( r = 0 ; r < MAX ; r++ ) {
+        for ( c = 0 ; c < 4 ; c++ ) {
+            a[r][c] = rand() % 100;
+        }
+    }
+    print( a );
+    printf("\n");
+    qsort( a, MAX, sizeof a[0], cmp );
+    print( a );
     return 0;
 }
